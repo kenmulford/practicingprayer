8f54758829819bae62695fb90744e47fa69bddfc
ken mulford <signup@kenmulford.com>
2025-10-11 15:08:43 -0400

    building out shell, setting up views, added icons

---
+++ PrayerApp/Views/CategoryMainView.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace PrayerApp.Views
+{
+    public partial class CategoryMainView : ContentPage
+    {
+        int count = 0;
+
+        public CategoryMainView()
+        {
+            InitializeComponent();
+        }
+    }
+}
--- PrayerApp/Views/MainPage.xaml.cs
+++ PrayerApp/Views/MainPage.xaml.cs
@@ -1,4 +1,4 @@
-﻿namespace PrayerApp
+﻿namespace PrayerApp.Views
 {
     public partial class MainPage : ContentPage
     {
