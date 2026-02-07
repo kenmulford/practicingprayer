a9473719f35c1eeab8d5b5b16ad5d54cfacb907f
Ken Mulford <ken@kenmulford.com>
2026-02-07 14:08:11 -0500

    got rid of underline default style on editor/entry android views

--- PrayerApp/AppShell.xaml.cs
+++ PrayerApp/AppShell.xaml.cs
@@ -1,14 +1,36 @@
-﻿using PrayerApp.Views.Prayer;
+﻿#if ANDROID
+using Android.Content.Res;
+using Android.Graphics;
+using AndroidX.AppCompat.Widget;
+#endif
+using PrayerApp.Views.Prayer;
 using PrayerApp.Views.PrayerCard;
 
 namespace PrayerApp
-
 {
     public partial class AppShell : Shell
     {
         public AppShell()
         {
             InitializeComponent();
+
+#if ANDROID
+            Microsoft.Maui.Handlers.EntryHandler.Mapper.AppendToMapping("NoUnderline", static (handler, view) =>
+            {
+                if (handler.PlatformView is AppCompatEditText editText)
+                {
+                    editText.SupportBackgroundTintList = ColorStateList.ValueOf(Android.Graphics.Color.Transparent);
+                }
+            });
+
+            Microsoft.Maui.Handlers.EditorHandler.Mapper.AppendToMapping("NoUnderline", static (handler, view) =>
+            {
+                if (handler.PlatformView is AppCompatEditText editText)
+                {
+                    editText.SupportBackgroundTintList = ColorStateList.ValueOf(Android.Graphics.Color.Transparent);
+                }
+            });
+#endif
 
             // register explicit routes for child pages
             Routing.RegisterRoute(nameof(PrayerCardPage), typeof(PrayerCardPage));
