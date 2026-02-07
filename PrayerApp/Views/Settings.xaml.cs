96d08bc54320220d61e8958cba5590b610129b49
Ken Mulford <ken@kenmulford.com>
2026-02-07 12:45:56 -0500

    wired up app setting for notifications to the settings property

--- PrayerApp/Views/Settings.xaml.cs
+++ PrayerApp/Views/Settings.xaml.cs
@@ -5,11 +5,23 @@ public partial class Settings : ContentPage
 	public Settings()
 	{
 		InitializeComponent();
+        OnAppearing();
 	}
+
+    protected override void OnAppearing()
+    {
+        base.OnAppearing();
+        chkSettingsAllowNotifications.IsToggled = PrayerApp.Services.Settings.AllowNotifications;
+    }
 
     private void btnClearSettings_Clicked(object sender, EventArgs e)
     {
         PrayerApp.Services.Settings.ClearSettings();
         DisplayAlertAsync("Settings Cleared", "The next time the app runs all DB info will be reset.", "OK");
+    }
+
+    private void chkSettingsAllowNotifications_Toggled(object sender, ToggledEventArgs e)
+    {
+        PrayerApp.Services.Settings.AllowNotifications = e.Value;
     }
 }
